9a1a8d952b9b027c2510c722b7495e4917f6bd85
tajpuriya27 <tajpuriyasunil27@gmail.com>
2024-01-13 14:03:52 +0545

    feat: sidebar updated

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,26 +1,73 @@
-import React from "react";
+import {
+  BsFileEarmarkText,
+  BsPersonFill,
+  BsGearFill,
+  BsJournals,
+  BsTagFill,
+  BsFillXCircleFill,
+  BsFileRuledFill,
+  BsFillPersonVcardFill,
+  BsFillCloudDownloadFill,
+  BsPercent,
+  BsEscape,
+} from "react-icons/bs";
 
 const Sidebar = () => {
   return (
-    <div className="sidebar">
-      Menu
+    <aside className="sidebar">
+      <p>Menu</p>
+      <hr />
       <ul>
-        <li>Invoices</li>
-        <li>Customers</li>
-        <li>My Business</li>
-        <li>Invoice Journal</li>
-        <li>Price List</li>
-        <li>Multiple Invoice</li>
-        <li>Price List</li>
-        <li>Multiple Invoicing</li>
-        <li>Unpaid Invoices</li>
-        <li>Offer</li>
-        <li>Inventory Control</li>
-        <li>Member Invoicing</li>
-        <li>Import/Export</li>
-        <li>Log out</li>
+        <li>
+          <BsFileEarmarkText className="icon-1" />
+          Invoices
+        </li>
+        <li>
+          <BsPersonFill className="icon-true" />
+          Customers
+        </li>
+        <li>
+          <BsGearFill className="icon-1" />
+          My Business
+        </li>
+        <li>
+          <BsJournals className="icon-1" />
+          Invoice Journal
+        </li>
+        <li>
+          <BsTagFill className="icon-2" />
+          Price List
+        </li>
+        <li>
+          <BsFileEarmarkText className="icon-1" />
+          Multiple Invoice
+        </li>
+        <li>
+          <BsFillXCircleFill className="icon-3" />
+          Unpaid Invoices
+        </li>
+        <li>
+          <BsPercent className="icon-1" />
+          Offer
+        </li>
+        <li>
+          <BsFileRuledFill className="icon-1" />
+          Inventory Control
+        </li>
+        <li>
+          <BsFillPersonVcardFill className="icon-5" />
+          Member Invoicing
+        </li>
+        <li>
+          <BsFillCloudDownloadFill className="icon-1" />
+          Import/Export
+        </li>
+        <li>
+          <BsEscape className="icon-4" />
+          Log out
+        </li>
       </ul>
-    </div>
+    </aside>
   );
 };
 
